28cad7bc58879d2842494ff40d968b4afe62d2df
dablro12 <dablro1232@gmail.com>
2024-03-14 21:09:17 +0900

    updated baseline_v2

--- train/run.sh
+++ train/run.sh
@@ -1,12 +1,15 @@
-python ./run.py --model "unet" \
-                --version "v1" \
-                --cuda "0"\
-                --ts_batch_size 2\
-                --vs_batch_size 2\
-                --epochs 200\
-                --loss "bce"\
-                --optimizer "AdamW"\
-                --learning_rate 0.001\
-                --scheduler "lambda"\
-                --pretrain "no" --pretrained_model "practice" --error_signal no\
-                --wandb "yes"\
+nohup python ./run.py --model "unet" \
+                      --version "v1" \
+                      --cuda "0" \
+                      --ts_batch_size 80 \
+                      --vs_batch_size 40 \
+                      --epochs 200 \
+                      --loss "bce" \
+                      --optimizer "AdamW" \
+                      --learning_rate 0.001 \
+                      --scheduler "lambda" \
+                      --pretrain "no" \
+                      --pretrained_model "practice" \
+                      --error_signal no \
+                      --wandb "yes" \
+                      > output.log 2>&1 &
