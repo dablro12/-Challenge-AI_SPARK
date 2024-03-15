e8f0761384ffa99484063bdef8946c57af961815
dablro12 <dablro1232@gmail.com>
2024-03-15 00:37:07 +0000

    update test code

--- test/utils/dataset.py
+++ test/utils/dataset.py
@@ -1,11 +1,47 @@
 from torch.utils.data import Dataset
 from torchvision import transforms
+import torch 
 import os 
 from PIL import Image
 import numpy as np
 import pandas as pd 
 import rasterio
 
+class Custom_test_Dataset(Dataset):
+    def __init__(self, csv_path, transform = None, MAX_PIXEL_VALUE = 65535, band = None): #transform 가지고올거있으면 가지고 오기
+        self.MAX_PIXEL_VALUE = MAX_PIXEL_VALUE 
+        self.band = band
+        self.transform = transform
+        self.image_paths= self.csv_load(csv_path = csv_path, type = csv_path.split('/')[-1].split('_')[0])
+    
+    def csv_load(self, csv_path, type):
+        self.type = type 
+        df = pd.read_csv(csv_path)
+        # os.pathconf('../')
+        image_paths = f'../../dataset/{type}_img/' + df[f'{type}_img']
+        return  image_paths
+        
+    def get_img_arr(self, path):
+        # img = rasterio.open(path).read().transpose((1, 2, 0))
+        img = rasterio.open(path).read(self.band).transpose((1, 2, 0))
+        img = np.float32(img)/self.MAX_PIXEL_VALUE
+
+        return img
+
+    def __len__(self):
+        return len(self.image_paths) 
+    
+    def __getitem__(self, idx):
+        image_path = self.image_paths[idx]
+        images = self.get_img_arr(path = image_path)
+        # transform 
+        if self.transform:
+            images = self.transform(images)
+        
+        return images, image_path #image name 필요함
+    
+    
+    
 class CustomDataset(Dataset):
     def __init__(self, csv_path, transform = None, MAX_PIXEL_VALUE = 65535, band = None): #transform 가지고올거있으면 가지고 오기
         self.MAX_PIXEL_VALUE = MAX_PIXEL_VALUE 
